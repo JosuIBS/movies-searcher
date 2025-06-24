31245e3af4fb2f98ceb698e40673b7c027f71efb
unknown <upnalab@eim-por-71425.dyc.a.unavarra.es>
2025-06-24 17:31:50 +0200

    Test: se muestra la película Inception cuando se busca por título

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,20 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { describe, it, expect } from "vitest";
-import App from "./App";
+import { App } from "./App.jsx";
 
 describe("App Component", () => {
-  it("displays the subtitle", () => {
+  it.only("Se muestra la película Inception cuando se busca por título", async () => {
+    render(<App />);
+
+    const input = screen.getByPlaceholderText(/buscar películas por título/i);
+    await userEvent.type(input, "Inception");
+
+    const movie = await screen.getByText("Inception");
+    expect(movie).toBeInTheDocument();
+  });
+
+  it("Comprobamos que no se muestran resultados si no hay incidencias", () => {
     render(<App />);
     const subtitle = screen.getByText(/encuentra tu película favorita/i);
     expect(subtitle).toBeInTheDocument();
